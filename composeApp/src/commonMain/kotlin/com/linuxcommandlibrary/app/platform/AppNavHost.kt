e3e69c53fdeaad6dd42dff894114c614b1198df4
Simon Schubert <simon@librem.one>
2026-04-28 15:13:38 +0200

    Simplify actual/expact nav implementations

--- composeApp/src/commonMain/kotlin/com/linuxcommandlibrary/app/platform/AppNavHost.kt
+++ composeApp/src/commonMain/kotlin/com/linuxcommandlibrary/app/platform/AppNavHost.kt
@@ -1,12 +1,23 @@
 package com.linuxcommandlibrary.app.platform
 
+import androidx.compose.animation.EnterTransition
+import androidx.compose.animation.ExitTransition
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
 
 @Composable
-expect fun AppNavHost(
+fun AppNavHost(
     navController: NavHostController,
     startDestination: Any,
     builder: NavGraphBuilder.() -> Unit,
-)
+) {
+    NavHost(
+        navController = navController,
+        startDestination = startDestination,
+        popEnterTransition = { EnterTransition.None },
+        popExitTransition = { ExitTransition.None },
+        builder = builder,
+    )
+}
